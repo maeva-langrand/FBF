c6e6e809d44a9fbc3eaa1a702262f378d0d3ca01
Maeva Langrand <maeva.langrand@oclok.school>
2026-03-27 10:13:40 +0100

    config mise en ligne

--- app/database/client-database.js
+++ app/database/client-database.js
@@ -1,14 +1,17 @@
-// j'ai besoin du module pg
-import { Client } from "pg";
+import pg from "pg";
+const { Pool } = pg;
 
-// je crée et je configure le client
-export const client = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME,
-});
-
-// ouvrir la connexion vers la bdd
-await client.connect();
+export const client = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        database: process.env.DB_NAME,
+      }
+);
